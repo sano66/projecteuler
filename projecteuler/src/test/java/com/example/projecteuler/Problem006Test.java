a6ab7a26a0d40c29f8af39fc7ab85ddfa8ebf7ca
sano66 <sano66@gmail.com>
2011-12-26 17:33:19 +0900

    2nd import

---
+++ projecteuler/src/test/java/com/example/projecteuler/Problem006Test.java
@@ -0,0 +1,33 @@
+package com.example.projecteuler;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.junit.Test;
+
+public class Problem006Test {
+	private static Log log = LogFactory.getLog(Problem006Test.class);
+
+	@Test
+	public void testProblem006() {
+		//
+		_assertEquals(25164150, (new Problem006()).getAnswer());
+	}
+
+	private void _assertEquals(int expect, int actual) {
+		_assertEquals(null, expect, actual);
+	}
+
+	private void _assertEquals(String msg, int expect, int actual) {
+		DateTime begin = new DateTime();
+		assertEquals(expect, actual);
+		DateTime end = new DateTime();
+		Duration dur = new Duration(begin, end);
+		String logMsg = StringUtils.isEmpty(msg) ? "" : msg;
+		log.debug(logMsg + " Elapsed: " + dur.getMillis() + " ms");
+	}
+}
