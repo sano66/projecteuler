6e0c74e5340148f00f9f89e95a1f4602dcc443f6
sano66 <sano66@gmail.com>
2011-12-28 16:32:38 +0900

    Solved Problem 008 and modify commons-lang version in pom.xml

---
+++ projecteuler/src/test/java/com/example/projecteuler/Problem008Test.java
@@ -0,0 +1,13 @@
+package com.example.projecteuler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Problem008Test {
+	@Test
+	public void testProblem008() {
+		//
+		assertEquals(40824, (new Problem008()).getAnswer());
+	}
+}
